b35bc5108e91a5c2380b299231ef98c9bcef3eeb
multivitamine <mike.achternaam@gmail.com>
2023-06-19 12:06:34 +0200

    clean directory

--- components/contentTypes/body.js
+++
@@ -1,6 +0,0 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-function Body(props) {
-    const { html } = props;
-    return <div>{documentToReactComponents(html)}</div>;
-}
-export default Body;
--- components/contentTypes/title.js
+++
@@ -1,5 +0,0 @@
-function Title(props) {
-    const { text } = props;
-    return <h2>{text}</h2>;
-}
-export default Title;
